578bffef47e5d64be0cbf44ec5eac782b0c344e9
Alla <molochko.1979@mail.ru>
2023-03-26 16:11:01 +0300

    add finish zadacha1

--- zadacha1/Program.cs
+++ zadacha1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int a = 5; 
+int b = 7;
+int.TryParse(Console.ReadLine(), out a);
+int.TryParse(Console.ReadLine(), out b);
+if (a>b)
+{
+  System.Console.WriteLine(a);
+} 
+else
+ 
+{
+    System.Console.WriteLine(b);
+}
