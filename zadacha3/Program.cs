53be498d13e4432edc000d4c466ed4cb5e93f0b8
Alla <molochko.1979@mail.ru>
2023-03-26 16:26:18 +0300

    add finish zadacha3

--- zadacha3/Program.cs
+++ zadacha3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+double a = 4; 
+double.TryParse(Console.ReadLine(), out a);
+
+if (a%2 ==0)
+
+{
+  Console.WriteLine("число чётное");
+}
+
+else
+ 
+{
+  Console.WriteLine("число нечётное");
+}
