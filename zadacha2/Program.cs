4a5e1e8bdeb27020ea408d3648a1a36c0d312b5a
Alla <molochko.1979@mail.ru>
2023-03-26 16:15:43 +0300

    add folder zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int a=2;
+int b=3;
+int c=7;
+int.TryParse(Console.ReadLine(), out a);
+int.TryParse(Console.ReadLine(), out b);
+int.TryParse(Console.ReadLine(), out c);
+if (a>b)
+{
+  System.Console.WriteLine(a);
+}
+if (c>a)
+{
+  System.Console.WriteLine(c);
+}
+
